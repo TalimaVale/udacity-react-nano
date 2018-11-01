25b3ed76db0b3aebbcbfcb21edf72e517fba7b4d
root <business.teamTALIMA@gmail.com>
2018-11-01 23:14:51 +0000

    Inital commit

---
+++ src/MovieCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import UserDisplay from './UserDisplay';
+
+class MovieCard extends Component {
+  render() {
+    const { movieInfo, usersWhoFavorited, users } = this.props;
+    
+    let content = [];
+    if(usersWhoFavorited){
+      content = <ul>
+        {usersWhoFavorited.map(userID => (
+      	  <UserDisplay key={userID} user={users[userID]} />
+      	))}
+    	</ul>
+    } else {
+      content = <p className="not-fav">This is not a favorite movie</p>;
+    }
+    
+    return <li className="movie-li" key={movieInfo.id}><h3 className="movie-title">{movieInfo.name}</h3>{content}</li>;
+  }
+}
+
+export default MovieCard;
